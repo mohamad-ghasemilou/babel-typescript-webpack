8ca97b9490f8582a8fdc3bf13515003a9ec71138
Mohamad-Ghasemilou <mohamad.ghasemilou@gmail.com>
2022-12-17 05:38:02 +0330

    add JavaScript with counter functionality

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const counter = document.getElementById("counter");
+const incButton = document.getElementById("incrementBtn");
+
+let count = 0;
+counter.textContent = count.toString();
+
+incButton.onclick = () => {
+    count++;
+    counter.textContent = count.toString();
+}
